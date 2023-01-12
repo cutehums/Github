cf135839b68d97589f723c56134de2bbdaa70a86
hums <113456535+cutehums@users.noreply.github.com>
2023-01-13 00:11:52 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    long long x; long long y; long long z; cin >> x >> y >> z;
+    cout << x + y + z;
+    return 0;
+}
+
