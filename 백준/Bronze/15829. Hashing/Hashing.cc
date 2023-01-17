32c87e3eb82977b8005c74e2fc38aa82febf8c5b
hums <113456535+cutehums@users.noreply.github.com>
2023-01-17 21:33:09 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -5,8 +5,7 @@ using namespace std;
 long long Mod = 1234567891;
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+
     int L; cin >> L;
     int ans = 0;
     for (int i = 0; i < L; i++){
@@ -19,6 +18,6 @@ int main(){
         ans += result;
         ans %= Mod;
     }
-    cout << ans;
+    printf("%lld\n", ans);
     return 0;
 }
