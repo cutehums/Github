86fb80daa7bc7e261b3e652f51ef7a8282f13c45
hums <113456535+cutehums@users.noreply.github.com>
2023-01-31 00:42:31 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+    // int hundred = 2;
+    // while (hundred--){
+    //     string str;
+    //     getline(cin, str, '1');
+    //     cout << str << '\n';
+    // }
+    int a; int b; cin >> a >> b;
+    cout << (a * a) - (b * b);
+
+    return 0;
+}
