1cc202fe57e8ab7cf744a7fa182fe6f26c97abb0
hums <113456535+cutehums@users.noreply.github.com>
2023-05-09 20:21:38 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 48 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(string args){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int n;
+	vector<pair<int,int> > v;
+	cin >> n;
+	while(n--){
+		int x,y;
+		cin >> x >> y;
+		v.push_back({x,y});
+	}
+	sort(v.begin(),v.end());
+	for(int i = 0; i < v.size(); i++)	cout << v[i].first << ' ' << v[i].second << '\n';
+}
