6aca13acdaf3b683f40ca3ea93ea952682b761e6
hums <113456535+cutehums@users.noreply.github.com>
2023-06-26 22:26:55 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string str;
+    int i;
+    cin >> str;
+    cin >> i;
+    cout << str[i-1];
+
+    return 0;
+}
+
