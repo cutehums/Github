913da4c72268cab85b5cdc238d639db0b1545e25
hums <113456535+cutehums@users.noreply.github.com>
2023-06-29 00:35:53 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 208 ms, Memory: 16284 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int N, M; cin >> N >> M;
+    map <string, string> m;
+    while (N--){
+        string address, password; cin >> address >> password;
+        m.insert({address, password});
+    }
+    while (M--){
+        string address; cin >> address;
+        cout << m.at(address) << '\n';
+    }
+
+    return 0;
+}
+
